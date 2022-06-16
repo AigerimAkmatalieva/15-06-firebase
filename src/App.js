b116bec8fba378253f23ebc9a09bf122cc5324a0
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2022-06-16 08:11:58 +0600

    add homework

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router";
+import ArticleCreate from './components/ArticleCreate';
+import ArticleList from './components/ArticleList';
 
 function App() {
-  return (
+  return (  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<ArticleList />}></Route>
+        <Route path="/create" element={<ArticleCreate />}></Route>
+      </Routes>
     </div>
   );
 }
